058a635f6fe51990f54448c796a6fc33ba60c2ab
nived-s <nivedsreenilayam@gmail.com>
2023-07-16 11:02:13 +0530

    final commit

---
+++ dice_roll/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+        title: const Text('Roll Dice'),
+        backgroundColor: Colors.cyan[600],
+      ),
+      body: const DiceRoll(),
+    ),
+  ));
+}
+
+class DiceRoll extends StatefulWidget {
+  const DiceRoll({super.key});
+
+  @override
+  State<DiceRoll> createState() => _DiceRollState();
+}
+
+class _DiceRollState extends State<DiceRoll> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(padding: const EdgeInsets.all(16)),
+              onPressed: () {
+                setState(() {
+                leftDiceNumber = Random().nextInt(6)+1; 
+                });
+              },
+              child: Image.asset('images/$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(padding: const EdgeInsets.all(16)),
+              onPressed: () {
+                setState(() {
+                rightDiceNumber = Random().nextInt(6)+1; 
+                });
+              },
+              child: Image.asset('images/$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
